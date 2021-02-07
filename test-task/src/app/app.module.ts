46c09f382380021c1600479dd157354d29657490
Urko <urkourkovich@gmail.com>
2021-02-07 14:12:07 +0200

    gh-pages

--- test-task/src/app/app.module.ts
+++ test-task/src/app/app.module.ts
@@ -1,34 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MyNavComponent } from './my-nav/my-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import {
+  JwPaginationComponent,
+  JwPaginationModule,
+} from 'jw-angular-pagination';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyNavComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    BrowserAnimationsModule,
+    NgxPaginationModule,
+    JwPaginationModule,
   ],
+  declarations: [AppComponent, JwPaginationComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
